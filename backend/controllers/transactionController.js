790db0c59004e0f11ffae8a2b73d74dfa5e54112
kosij <justinkoytb@gmail.com>
2023-02-07 09:08:30 +0000

    create all basic controller methods

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,121 @@
+const Transaction = require('../models/transactionModel');
+const User = require('../models/userModel');
+const mongoose = require('mongoose');
+
+// @route    POST api/transactions
+// @desc     Create a transaction
+exports.addTransaction = async (req, res) => {
+    const { date, description, amount, category } = req.body;
+
+    try {
+        const newTransaction = new Transaction({
+            date,
+            description,
+            amount,
+            category,
+            user: req.user.id
+        });
+
+        const transaction = await newTransaction.save();
+
+        // update user transactions
+        await User.findByIdAndUpdate(req.user.id, { $push: { transactions: transaction._id } });
+
+
+        res.json(transaction);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// @route    GET api/transactions
+// @desc     Get all transactions of a user
+exports.getTransactions = async (req, res) => {
+    try {
+        const transactions = await Transaction.find({ user: req.user.id }).sort({ date: -1 });
+
+        res.json(transactions);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// @route    GET api/transactions/:id
+// @desc     Get transaction by ID
+exports.getTransaction = async (req, res) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+
+        if (!transaction) return res.status(404).json({ msg: 'Transaction not found' });
+
+        res.json(transaction);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Transaction not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+};
+
+// @route    PATCH api/transactions/:id
+// @desc     Update a transaction
+exports.updateTransaction = async (req, res) => {
+    const { date, description, amount, category } = req.body;
+
+    // Build transaction object
+    const transactionFields = {};
+    if (date) transactionFields.date = date;
+    if (description) transactionFields.description = description;
+    if (amount) transactionFields.amount = amount;
+    if (category) transactionFields.category = category;
+
+    try {
+        let transaction = await Transaction.findById(req.params.id);
+
+        if (!transaction) return res.status(404).json({ msg: 'Transaction not found' });
+
+        // Make sure user owns transaction
+        if (transaction.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        transaction = await Transaction.findByIdAndUpdate(
+            req.params.id,
+            { $set: transactionFields },
+            { new: true }
+        );
+
+        res.json(transaction);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// @route    DELETE api/transactions/:id
+// @desc     Delete a transaction
+exports.deleteTransaction = async (req, res) => {
+    try {
+        let transaction = await Transaction.findById(req.params.id);
+
+        if (!transaction) return res.status(404).json({ msg: 'Transaction not found' });
+
+        // Make sure user owns transaction
+        if (transaction.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        await transaction.remove();
+
+        res.json({ msg: 'Transaction removed' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Transaction not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+};
---
+++ backend/controllers/userController.js
@@ -0,0 +1,113 @@
+const User = require('../models/userModel');
+
+// @route   POST /api/users
+// @desc    Create a user
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+};
+
+// @route   GET /api/users
+// @desc    Get all users
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            success: true,
+            count: users.length,
+            data: users
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+};
+
+// @route   GET /api/users/:id
+// @desc    Get a single user
+exports.getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                error: 'User not found'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+};
+
+// @route   PATCH /api/users/:id
+// @desc    Update a user
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                error: 'User not found'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+};
+
+// @route   DELETE /api/users/:id
+// @desc    Delete a user
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                error: 'User not found'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: err.message
+        });
+    }
+};
