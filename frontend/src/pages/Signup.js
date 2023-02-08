04c68667911db08afa2e26d8fcd852b82dc38ccb
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2023-02-08 19:48:37 +0000

    signup hook in progress

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,15 +1,17 @@
 import React from "react"
 import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
 
 const Signup = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(name, email, password)
+        await signup(email, password)
     }
 
     return (
@@ -35,7 +37,8 @@ const Signup = () => {
                 value={password}
             />
 
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign up</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 
