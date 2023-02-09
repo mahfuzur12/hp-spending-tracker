7bece0a73f336f0273576d75ab746d0d1fa14cbf
Bebsi5 <99485705+Bebsi5@users.noreply.github.com>
2023-02-09 21:10:25 +0000

    Add the API
    Create button to connect user to banks and display there info when logged in.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 // pages + components
 import Overview from "./pages/Overview";
 
--- frontend/src/pages/Overview.js
+++ frontend/src/pages/Overview.js
@@ -1,9 +1,64 @@
-const Overview = () => {
-    return (
-        <div className="overview">
-            <h1>Overview</h1>
-        </div>
-    );
+//import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+import { usePlaidLink } from 'react-plaid-link';
+//import './App.css';
+
+axios.default.baseUrl = "http://localhost:8000"
+
+function PlaidAuth({publicToken}) {
+  const [account, setAccount] = useState();
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    async function fetch() {
+        let accessToken = await axios.post("http://localhost:8000/exchange_public_token", {public_token: publicToken});
+        console.log("accessToken", accessToken.data);
+        const auth = await axios.post("http://localhost:8000/auth", {access_token: accessToken.data.accessToken});
+        console.log("auth", auth.data);
+        setAccount(auth.data.numbers.bacs[0]);
+        setBalance(auth.data.accounts[0].balances);
+    } 
+    fetch();
+  }, []);
+  return account && balance &&(
+    <>
+      <p>Account Number: {account.account}</p>
+      <p>Sort Code: {account.sort_code}</p>
+      <p>Balance: {balance.current}</p>
+    </>
+  );
+}
+
+function Overview() {
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+
+  useEffect(() => {
+
+    async function fetch() {
+        const response = await axios.post("http://localhost:8000/create_link_token")
+        setLinkToken(response.data.link_token);
+    }
+    fetch();
+  }, []);
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+      setPublicToken(public_token);
+      console.log("success", public_token, metadata);
+      // send public_token to server
+    },
+  });
+  
+  return publicToken ? (<PlaidAuth publicToken={publicToken} />) : (
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
+  );
+
 }
 
 export default Overview;
