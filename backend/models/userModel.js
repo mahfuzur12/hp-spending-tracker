a2e83a11b768661f94cc56606c3afe3189892aef
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2023-02-14 20:59:16 +0000

    undo changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,34 +25,6 @@ const UserSchema = new Schema({
     ]
 });
 
-UserSchema.statics.signup = async function (name, email, password) {
-
-    if (!name || !email || !password) {
-        throw Error('All fields must be filled')
-    }
-    if (!validator.isEmail(email)) {
-        throw Error('Email is not valid')
-    }
-    if(!validator.isStrongPassword(password)) {
-        throw Error('Password is not strong enough')
-    }
-
-
-    const exists = await this.findOne({ email })
-
-    if (exists) {
-        throw Error('This email is already in use')
-    }
-
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const user = await this.create({ name, email, password: hash })
-
-    return user
-
-}
-
 UserSchema.statics.login = async function (email, password) {
     if (!email || !password) {
         throw Error('All fields must be filled!')
