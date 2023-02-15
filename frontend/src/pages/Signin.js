835587cbc149e745e018aa46738d3eda1a35575b
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2023-02-15 22:33:23 +0000

    add password visibility

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import { MdVisibility } from "react-icons/md";
+import { MdVisibilityOff } from "react-icons/md";
 import Input from "../components/Input/Input";
 
 const Signin = () => {
-    return (
-      <form>
-        <Input type="email" text="Email" />
-        <Input type="password" text="Password" />
-        <div>
-            <button>Signin</button>
-        </div>
-      </form>
-    );
+
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!visible);
   };
-  
-  export default Signin;
+
+  return (
+    <form>
+      <Input type="email" text="Email" />
+      <Input
+        type={visible ? "text" : "password"}
+        icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
+        text="Password"
+        handleClick={handleClick}
+      />
+      <div>
+        <button>Signin</button>
+      </div>
+    </form>
+  );
+};
+
+export default Signin;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Input from "../components/Input/Input";
+import { MdVisibility } from "react-icons/md";
+import { MdVisibilityOff } from "react-icons/md";
+import { useState } from "react";
 
 const Signup = () => {
+
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!visible);
+  };
+
   return (
     <form>
       <Input type="text" text="Name" />
       <Input type="text" text="Email" />
-      <Input type="password" text="Password" />
-      <Input type="password" text="Confirm Password" />
+      <Input
+        type={visible ? "text" : "password"}
+        icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
+        text="Password"
+        handleClick={handleClick}
+      />
+      <Input
+        type={visible ? "text" : "password"}
+        icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
+        text="Confirm Password"
+        handleClick={handleClick}
+      />
       <div>
         <button>Signup</button>
       </div>
