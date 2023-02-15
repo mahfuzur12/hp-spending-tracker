b5c7733993dbfa92b346a5a6e33d559719efefaf
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2023-02-15 21:48:31 +0000

    Add overview page

--- frontend/src/Layouts/ProfileLayout/ProfileLayout.js
+++ frontend/src/Layouts/ProfileLayout/ProfileLayout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Overview from "../../pages/Overview";
 
 const ProfileLayout = () => {
     return (
@@ -8,6 +9,8 @@ const ProfileLayout = () => {
         <Navbar />
 
         <Sidebar />
+
+        <Overview />
       </div>
     );
   };
