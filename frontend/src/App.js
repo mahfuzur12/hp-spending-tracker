ab8e4f67e589555bd498575a98d0d8b00780dea8
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2023-02-16 15:20:09 +0000

    connect signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ActivateLayout from "./Layouts/ActivateLayout/ActivateLayout";
 import AuthLayout from './Layouts/AuthLayout/AuthLayout'
 import ProfileLayout from "./Layouts/ProfileLayout/ProfileLayout";
 import ResetLayout from "./Layouts/ResetLayout/ResetLayout";
 import { AuthContext } from "./context/AuthContext";
-
+import axios from "axios";
 
 function App() {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { dispatch, token, isLoggedIn } = useContext(AuthContext);
+
+  // get ac token
+  useEffect(() => {
+    const _appSigning = localStorage.getItem("_appSigning");
+    if (_appSigning) {
+      const getToken = async () => {
+        const res = await axios.post("/access", null);
+        dispatch({ type: "GET_TOKEN", payload: res.data.ac_token });
+      };
+      getToken();
+    }
+  }, [dispatch, isLoggedIn]);
+
+  // get user data
+  useEffect(() => {
+    if (token) {
+      const getUser = async () => {
+        dispatch({ type: "SIGNING" });
+        const res = await axios.get("/user", {
+          headers: { Authorization: token },
+        });
+        dispatch({ type: "GET_USER", payload: res.data });
+      };
+      getUser();
+    }
+  }, [dispatch, token]);
 
   return (
     <Router>
--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,30 +1,78 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 import { MdVisibility } from "react-icons/md";
 import { MdVisibilityOff } from "react-icons/md";
+import { isEmpty, isEmail } from "../components/helper/validate";
+import axios from 'axios'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Input from "../components/Input/Input";
+import { AuthContext } from "../context/AuthContext";
+
+const initialState = {
+  name: '',
+  password: ''
+}
 
 const Signin = () => {
 
   const [visible, setVisible] = useState(false);
+  const [data, setData] = useState(initialState)
+  const {email, password} = data
+  const {dispatch} = useContext(AuthContext)
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleClick = () => {
     setVisible(!visible);
   };
 
+  const signin = async (e) => {
+    e.preventDefault();
+    // check fields
+    if (isEmpty(email) || isEmpty(password))
+      return toast("Please fill in all fields.", {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    // check email
+    if (!isEmail(email))
+      return toast("Please enter a valid email addresss.", {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    try {
+      await axios.post("/signin", { email, password });
+      localStorage.setItem("_appSigning", true);
+      dispatch({ type: "SIGNING" });
+    } catch (err) {
+      toast(err.response.data.msg, {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    }
+  };
+
   return (
-    <form>
-      <Input type="email" text="Email" />
+    <>
+    <ToastContainer />
+    <form onSubmit={signin}>
+      <Input type="email" text="Email" name="email" handleChange={handleChange}/>
       <Input
+        name="password"
         type={visible ? "text" : "password"}
         icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
         text="Password"
         handleClick={handleClick}
+        handleChange={handleChange}
       />
       <div>
-        <button>Signin</button>
+        <button type="submit">Signin</button>
       </div>
     </form>
+    </>
   );
 };
 
