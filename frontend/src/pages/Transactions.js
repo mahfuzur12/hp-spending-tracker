9a71b9f29583c5c1c0bcc22a29f26e5a0a091ba9
“Goncalo <goncalo.azevedo@kcl.ac.uk>
2023-03-13 12:35:02 +0000

    Transactions page update

--- frontend/src/pages/Transactions.js
+++ frontend/src/pages/Transactions.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 import React,{useEffect, useState} from "react";
 import "../transactions.css"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+
+
 
 function Transactions() {
     const [transactions, setTransactions] = useState([]);
+    const [description, setDescription] = useState(" ");
+    const [amount, setAmount] = useState(" ");
+    const [category, setCategory] = useState(" ");
+     const [date, setDate] = useState(" ");
+   
     
-    useEffect(() => {
+    useEffect(function renderTransactions() {
        
             axios.get("http://localhost:8000/api/transactions")
             .then(res =>    {
@@ -16,41 +26,99 @@ function Transactions() {
             console.log(err);
         })
     }, []);
-  
+
+    useEffect(function editTransaction(id){
+        axios.get(`http://localhost:8000/api/transactions/${id}`)
+        .then(res => {
+            console.log(res.data);
+            setDescription(res.data.description);
+            setAmount(res.data.amount);
+            setCategory(res.data.category);
+            setDate(res.data.date);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        
+
+    }, []);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.patch(`http://localhost:8000/api/transactions`, {
+            description,
+            amount,
+            category,
+            date
+
+        })
+        .then(res => {
+            console.log(res.data);
+        }
+        )
+        .catch(err => {
+            console.log(err);
+        }
+        )
+
+          
+    }
+
     
 
-return(
-    <div>
-    <table> 
-        <thead>
-            <tr>
-         <th >DESCRIPTION</th> 
-         <th >AMOUNT SPENT</th> 
-         <th >CATEGORY</th> 
-         <th >DATE</th> 
-         </tr>
-        </thead>
-        <tbody>
        
-            
-                        
-            {transactions.map((transaction) => ( 
-                <tr >
-                <td>{transaction.description}</td>
-                <td>{transaction.amount} £</td>
-                <td>{transaction.category}</td>
-                <td>{transaction.date.toString().substring(0,10)}</td>
+return(
+   
+    <div className="transactions">
+    <div className='div1'>
+        <table>
+            <thead>
+                <tr>
+                    <th>DESCRIPTION</th>
+                    <th>AMOUNT <br></br> SPENT</th>
+                    <th>CATEGORY</th>
+                    <th>DATE</th>
                 </tr>
-            ))}
+            </thead>
+            <tbody>
+                {transactions.map((transaction) => (
+                    <tr>
+                        <td > <Popup trigger={<button className='edit-btn'> Edit </button>} hideOnDocumentClick={true} >
+    <div className="modal">
+        <div className="modal-content">
+            <h2>Edit Transaction</h2>
+            <span className="close">&times;</span>
+        <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
+                <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
+                <button type="submit">SAVE</button>
 
-        </tbody>
-       </table>
-      
+            </form>
+            </div>
+            </div>
+                        </Popup>{transaction.description}</td>
+                        <td >{transaction.amount} £</td>
+                        <td>{transaction.category}</td>
+                        <td>{transaction.date.toString().substring(0, 10)}</td>
+                    </tr>
+                ))}
+
+            </tbody>
+        </table>
+
+            
     </div>
+    
+
+
+    </div>
+
+
 
 )
 }
 
 
 export default Transactions;
-    
