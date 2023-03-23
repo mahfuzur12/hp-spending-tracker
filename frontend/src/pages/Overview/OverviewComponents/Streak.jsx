d497b020bc46d327636f1eead7beb9dbd22c6cda
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2023-03-23 14:46:02 +0000

    streak message

--- frontend/src/pages/Overview/OverviewComponents/Streak.jsx
+++ frontend/src/pages/Overview/OverviewComponents/Streak.jsx
@@ -16,26 +16,28 @@ const Title = styled.h2`
   font-family: ${theme.fonts.titles};
   font-size: ${theme.fontSizes.medium};
   font-weight: ${theme.fontWeight.semiBold};
-    color: ${theme.colors.text} !important;
+  color: ${theme.colors.text} !important;
   margin-bottom: 1vh;
 `;
 
 const Description = styled.p`
   font-family: ${theme.fonts.normalText};
   font-size: ${theme.fontSizes.normalText};
-    font-weight: ${theme.fontWeight.regular};
-    color: ${theme.colors.text};
+  font-weight: ${theme.fontWeight.regular};
+  color: ${theme.colors.text};
   text-align: left;
   margin-bottom: 2vh;
 `;
 
 const Streak = ({ streak }) => {
-
-
   return (
     <Container>
       <Title>Streaks</Title>
-
+      {streak === 0 ? (
+        <Description>You have no streak!</Description>
+      ) : (
+        <Description>Great! You have a streak </Description>
+      )}
       <StreaksNavbar streak={streak} />
     </Container>
   );
